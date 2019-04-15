6d4166d68979f0974e03958088e791d61fe096be
Shiva Ramoudith <shivaramoudith@Shivas-MacBook-Pro.local>
2019-04-15 15:51:32 -0400

    Converted main image to webp format for faster loading.

--- dns_cache/dns_cache/settings_dev.py
+++ dns_cache/dns_cache/settings_dev.py
@@ -60,7 +60,7 @@ WSGI_APPLICATION = 'dns_cache.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'database.db.sqlite3'),
     }
 }
 
